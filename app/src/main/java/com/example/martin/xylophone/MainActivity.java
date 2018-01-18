c2af01c7409f327c7befc43f8f3e9dd30ce1b505
marb61a <marb61a@gmail.com>
2018-01-18 13:44:55 +0000

    updated main activity with initial soundpool variables and methods

--- app/src/main/java/com/example/martin/xylophone/MainActivity.java
+++ app/src/main/java/com/example/martin/xylophone/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.martin.xylophone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.media.SoundPool;
+import android.media.AudioManager;
+import android.util.Log;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    // Helpful Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // Member Variables
+    private SoundPool mSoundPool;
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creates a new soundpool
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    // Individual methods linked to onClick properties in the XML layout file
+    public void playC(View v){
+        Log.d("Xylophone", "Red Button has been clicked")
     }
 }
